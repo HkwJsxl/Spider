219073166d7386eb74189a3117c60e72c44b5ff9
韩科伟 <88998157+18533538210@users.noreply.github.com>
2022-03-16 19:33:53 +0800

    Update 网易云榜单.py

--- Demo/网易云榜单.py
+++ Demo/网易云榜单.py
@@ -1,16 +1,3 @@
-# http://music.163.com/song/media/outer/url?id=
-# 网易：https://music.163.com/#/song?id=25906124
-# ＱＱ：https://y.qq.com/n/yqq/song/002B2EAA3brD5b.html
-# 酷狗：http://www.kugou.com/song/#hash=08228af3cb404e8a4e7e9871bf543ff6
-# 酷我：https://www.kuwo.cn/play_detail/175264544/
-# 千千：https://music.taihe.com/song/266069
-# 一听：https://www.1ting.com/player/b6/player_357838.html
-# 咪咕：https://music.migu.cn/v3/music/song/477803
-# 荔枝：https://www.lizhi.fm/1947925/2498707770886461446
-# 蜻蜓：https://www.qingting.fm/channels/158696/programs/5266259
-# 喜马拉雅：https://www.ximalaya.com/51701370/sound/24755731
-# 5sing原创：http://5sing.kugou.com/yc/3082899.html
-# 5sing翻唱：http://5sing.kugou.com/fc/14369766.html
 import requests
 import re
 
@@ -21,16 +8,12 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
 }
 data_html = requests.get(url=url, headers=headers).text
-# <li><a href="/song?id=1901371647">孤勇者</a></li>
-# <li><a href="/song?id=488249475">哪里都是你</a></li>
-# <li><a href="/song?id=1891469546">删了吧</a></li>
 data_music = re.findall('<li><a href="/song\?id=(.*?)">(.*?)</a></li>', data_html)
 i = 1
 for data in data_music:
     id_ = data[0]
     name = data[1]
     add = requests.get(url=f'https://music.163.com/song/media/outer/url?id={id_}').content
-    # print(id, name, add)
     with open(f'music\\{name}.mp3', mode='wb') as f:
         f.write(add)
     print(f'{i}---{name}---下载成功')
